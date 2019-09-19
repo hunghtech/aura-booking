378b86a8274c9b9b417eaa62098d2bcf148fc5fa
HungDo <hung.do@saigontechnology.com>
2019-09-19 15:26:49 +0700

    Update database

---
+++ database/aura-booking-2019_19-09.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Localhost
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : aura-booking-2019
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2019-09-19 15:26:26
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
